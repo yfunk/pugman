1513d265405f1604f2920064cc557acd7b9853d9
yfunk <14955991+yfunk@users.noreply.github.com>
2022-04-08 12:01:40 +0200

    implement app

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+
+// App variables
+const app = express();
+const port = process.env.PORT || "8000";
+
+// App configuration
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+// Routes definition
+app.get("/", (req, res) => {
+    res.render("index", { cache: true });
+});
+
+// Server activation
+app.listen(port, () => {
+    console.log(`Listening to requests on http://localhost:${port}`);
+  });
